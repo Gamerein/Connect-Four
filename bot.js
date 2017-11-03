a1474aa8026c40e7924ff6ef76d181646ad5597d
unknown <reinrijn@gmail.com>
2017-11-03 17:00:45 +0100

    update

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 //todo: give start attributes, like emoticon choices. 
-// Use the Game.present stuff
 // Make a help
-// Give message on `cf reset`
 
 var Discord = require('discord.io');
 
@@ -58,7 +56,7 @@ bot.on("message", function (user, userID, channelID, message, event)
         var command = message.substring(prefix.length + 1);
         command = command.toLowerCase();
 
-        if(command == "join")
+        if(command == "join" && Game.present == true)
         {
             bot.sendMessage({to: channelID, message: "`" + author + "` joined"})
             if (author != Game.player1 || author != Game.player1){
@@ -67,6 +65,8 @@ bot.on("message", function (user, userID, channelID, message, event)
             } else{
                 bot.sendMessage({to: channelID, message: "you have already joined"})
             }
+        } else if(command == "join" && Game.present != true) {
+            bot.sendMessage({to: channelID, message: "no game started"})
         }
     }
 
@@ -75,9 +75,11 @@ bot.on("message", function (user, userID, channelID, message, event)
         var command = message.substring(prefix.length + 1);
         command = command.toLowerCase();
 
-        if(command == "show")
+        if(command == "show" && Game.present == true)
         {
             draw();
+        } else if(command == "show" && Game.present == fasle){
+            bot.sendMessage({to: channelID, message: "no game started"});
         }
     }
     if (message.substring(0, prefix.length) == prefix) 
@@ -95,6 +97,7 @@ bot.on("message", function (user, userID, channelID, message, event)
             gs4 = [circle, circle, circle, circle, circle, circle, circle];
             gs5 = [circle, circle, circle, circle, circle, circle, circle];
             gs6 = [circle, circle, circle, circle, circle, circle, circle];
+            bot.sendMessage({to: channelID, message: "game resetted"})
         }
     }   
 
@@ -102,8 +105,8 @@ bot.on("message", function (user, userID, channelID, message, event)
     {
         var command = message.substring(prefix.length + 1);
         command = command.toLowerCase();        
-        if (command.includes("place") == true){
-            if(Game.lastPlaced != author){
+        if (command.includes("place") == true && Game.present == true){
+            if(Game.lastPlaced != author && Game.player1 == author || Game.lastPlaced != author && Game.player2 == author){
                 if(command.includes("red") == true)
                 {
                     if(command.includes("1") == true){place(red, 0);} 
@@ -122,19 +125,21 @@ bot.on("message", function (user, userID, channelID, message, event)
                     else if(command.includes("3") == true){place(blue, 2);}
                     else if(command.includes("4") == true){place(blue, 3);}
                     else if(command.includes("5") == true){place(blue, 4);}
-                    else if(command.includes("6") == true){place(blue, 5    );}
+                    else if(command.includes("6") == true){place(blue, 5);}
                 }
                 if (command.includes("blue") == false && command.includes("red") == false) {
                     bot.sendMessage({to: channelID, message: "No color given"});                
                 }
 
-                if (command.includes("1") == false && command.includes("2") == false && command.includes("3") == false && command.includes("4") == false){
+                if (command.includes("1") == false && command.includes("2") == false && command.includes("3") == false && command.includes("4") == false && command.includes("5") == false && command.includes("6") == false ){
                     bot.sendMessage({to: channelID, message: "No row given"});                
                 }
             } else{
-                bot.sendMessage({to:channelID, message: "You already placed!"})
+                bot.sendMessage({to:channelID, message: "You already placed!"});
             }
-        } 
+        } else if(command.includes("place") == true && Game.present != true){
+                bot.sendMessage({to: channelID, message: "No game started"});
+        }
     }
 
 
@@ -160,7 +165,19 @@ bot.on("message", function (user, userID, channelID, message, event)
     {
         var command = message.substring(prefix.length + 1);
         if (command.startsWith("eval") == true && author == "Gamerein"){
-            eval(command.substring(5));
+            try {eval(command.substring(5));}
+            catch(err){
+                bot.sendMessage({to: channelID, message:err})
+            }
+        }
+    }
+
+    if (message.substring(0, prefix.length) == prefix) 
+    {
+        var command = message.substring(prefix.length + 1);
+        command = command.toLowerCase();
+        if (command == "kill" && author == "Gamerein"){
+            throw "killed by " + author;
         }
     }
 
